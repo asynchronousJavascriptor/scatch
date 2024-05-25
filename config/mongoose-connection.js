56043875e24b4596caa5505ad1ab580f0ad9b295
Harsh Sharma <harshu8545@gmail.com>
2024-05-25 14:44:25 +0530

    23rd may

--- config/mongoose-connection.js
+++ config/mongoose-connection.js
@@ -1,12 +1,15 @@
 const mongoose = require("mongoose");
+const config = require("config");
+
+const dbgr = require("debug")("development:mongoose");
 
 mongoose
-  .connect("mongodb://127.0.0.1:27017/scatch")
+  .connect(`${config.get("MONGODB_URI")}/scatch`)
   .then(function () {
-    console.log("connected");
+    dbgr("connected");
   })
   .catch(function (err) {
-    console.log(err);
+    dbgr(err);
   });
 
 module.exports = mongoose.connection;
--- routes/ownersRouter.js
+++ routes/ownersRouter.js
@@ -1,5 +1,27 @@
 const express = require("express");
 const router = express.Router();
+const ownerModel = require("../models/owner-model");
+
+if (process.env.NODE_ENV === "development") {
+  router.post("/create", async function (req, res) {
+    let owners = await ownerModel.find();
+    if (owners.length > 0) {
+      return res
+        .status(500)
+        .send("You don't have permission to create a new owner.");
+    }
+
+    let { fullname, email, password } = req.body;
+
+    let createdOwner = await ownerModel.create({
+      fullname,
+      email,
+      password,
+    });
+
+    res.status(201).send(createdOwner);
+  });
+}
 
 router.get("/", function (req, res) {
   res.send("hey it's working");
